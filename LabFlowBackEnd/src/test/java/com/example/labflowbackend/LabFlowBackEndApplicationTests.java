1e7c01cecb52cbcba1fab910358c81f6819b4fe6
Sean-Salamah <seanhsalamah@gmail.com>
2026-09-23 14:39:02 +0100

    BackEnd Added

---
+++ LabFlowBackEnd/src/test/java/com/example/labflowbackend/LabFlowBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labflowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabFlowBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
